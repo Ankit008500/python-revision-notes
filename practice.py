4a272ceb828f586f2647fb9b8f0724552257508f
Ankit <25ugcen105@oc.svsu.ac.in>
2026-05-01 11:15:04 +0530

    Add practice.py with multiple examples and functions

---
+++ practice.py
@@ -0,0 +1,17 @@
+# Example 1
+x = 5
+print(x)
+
+# Example 2
+name = "Ankit"
+print("Hello", name)
+
+# Example 3
+for i in range(5):
+    print(i)
+
+# Example 4
+def add(a, b):
+    return a + b
+
+print(add(2, 3))
